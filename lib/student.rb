2f6e96052668650dbfdb4182520c2d50aabc4ff9
jose luis <cortezluisjose@gmail.com>
2019-10-09 19:25:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,17 +9,7 @@ class Student
     student
   end
 
-  def self.all
-   sql = <<-SQL
-   SELECT *
-    Student class	
-    FROM students
-    SQL
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    
-  end
-end
+  
   def self.find_by_name(name)
     sql = <<-SQL	    SELECT * 
     FROM students
@@ -30,6 +20,7 @@ end
       self.new_from_db(row)
     end.first
   end
+  
   def self.count_all_students_in_grade_9
   sql = <<-SQL
   SELECT *
